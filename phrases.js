c31f29c710c7eba2482fc8ec8b4e245f39e58d3b
Daniel Escoz <darkhogg@gmail.com>
2015-01-07 12:44:37 +0100

    First Commit

---
+++ phrases.js
@@ -0,0 +1,27 @@
+var phrases = [];
+
+/**
+ * Return all phrases available
+ */
+module.exports.all = function all () {
+    return phrases;
+};
+
+/**
+ * Return multiple random phrases
+ */
+module.exports.random = function random (n) {
+    var len = phrases.length;
+    n = Math.min(n || 1, len);
+
+    var result = new Array(n);
+    var taken = new Array(len);
+
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = phrases[x in taken ? taken[x] : x];
+        taken[x] = --len;
+    }
+
+    return result;
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+var express = require('express');
+
+var phrases = require('./phrases');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.json(phrases.all());
+});
+
+app.get('/random/:num?', function (req, res) {
+    res.json(phrases.random(req.params.num));
+});
+
+app.listen(process.env.PORT);
